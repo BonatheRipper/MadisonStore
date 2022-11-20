91f8983de55ff7157a1084c89f2f4eeaa95da805
Izeogu Maduabuchi <53067387+BonatheRipper@users.noreply.github.com>
2022-11-20 09:55:16 +0100

    modified app login serrvice

--- src/services/LoginUser.js
+++ src/services/LoginUser.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 export const getUser = async (email, password) => {
-  const { data } = await axios.post(`api/users/login`, {
-    email,
-    password,
-  });
+  const { data } = await axios.post(
+    `https://madison.bona9ja.online/api/users/login`,
+    {
+      email,
+      password,
+    }
+  );
   return data;
 };
